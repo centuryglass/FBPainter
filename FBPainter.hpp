84483ef0f4425b9cc5a49c5f439d6284fbf2b302
centuryglass <anthony0857@gmail.com>
2019-08-01 13:55:50 -0500

    Added library include header

---
+++ FBPainter.hpp
@@ -0,0 +1,11 @@
+/**
+ * @file  FBPainter.hpp
+ *
+ * @brief  The primary include file for the FBPainter library.
+ */
+#pragma once
+#include "Source/FrameBuffer.h"
+#include "Source/ImagePainter.h"
+#ifdef USE_PNG
+#include "Source/PngImage.h"
+#endif
